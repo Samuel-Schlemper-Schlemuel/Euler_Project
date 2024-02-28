1119b634f9d7503bf1dc772421a39568136d6da4
Samuel Schlemper <samuelschlemper2006@gmail.com>
2024-02-28 10:03:00 -0300

    52 feito

--- 50-59/53.js
+++ 50-59/53.js
@@ -1,6 +1,29 @@
 function combinatoricSelections(limit) {
+    result = 0
 
-    return 1
+    function factoralize(num){
+        let fact = 1
+
+        for(let mult = num; mult >= 2; mult--){
+            fact *= mult
+        }
+
+        return fact
+    }
+
+    function test(n, r){
+        return factoralize(n) / (factoralize(r) * factoralize(n - r))
+    }
+
+    for(let n = 1; n <= 100; n++){
+        for(let r = 1; r <= n; r++){
+            if(test(n, r) > limit){
+                result++
+            }
+        }
+    }
+
+    return result
 }
   
-combinatoricSelections(1000000)
+console.log(combinatoricSelections(1000000))
